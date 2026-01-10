20df8441bb9e1267cfd2dd1efff1bd385f606a77
FyslAhmd <info.faysal.32@gmail.com>
2026-01-11 00:22:02 +0600

    learn loading and not found page adding

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div className="text-center mt-10">
+            <h2 className="text-2xl font-semibold">Loading...</h2>
+        </div>
+    );
+};
+
+export default Loading;
---
+++ src/app/not-found.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NotFount = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center mt-10">
+        404 - Page Not Found
+      </h1>
+      <p className="text-center mt-4">
+        Sorry, the page you are looking for does not exist.
+      </p>
+    </div>
+  );
+};
+
+export default NotFount;
