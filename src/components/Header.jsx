8f160239c965c5fe6bff58e21bef9270158bcf04
FyslAhmd <info.faysal.32@gmail.com>
2026-01-10 23:58:55 +0600

    hide path using pathname

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,28 @@
-import Link from 'next/link';
-import React from 'react';
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 
 const Header = () => {
-    return (
-        <div className='px-4 py-3 border-b-2 border-gray-500 flex items-center justify-between flex-wrap'>
-            <Link href="/" className='text-xl font-semibold'>Dev Story</Link>
+  const pathname = usePathname();
+  if (pathname.startsWith("/dashboard")) {
+    return null;
+  }
+  return (
+    <div className="px-4 py-3 border-b-2 border-gray-500 flex items-center justify-between flex-wrap">
+      <Link href="/" className="text-xl font-semibold">
+        Dev Story
+      </Link>
 
-            <nav className='text-xl space-x-4'>
-                <Link href="/about">About</Link>
-                <Link href="/stories">Stories</Link>
-                <Link href="/tutorials">Tutorials</Link>
-                <Link href="/login">Login</Link>
-                <Link href="/register">Register</Link>
-            </nav>
-        </div>
-    );
+      <nav className="text-xl space-x-4">
+        <Link href="/about">About</Link>
+        <Link href="/stories">Stories</Link>
+        <Link href="/tutorials">Tutorials</Link>
+        <Link href="/login">Login</Link>
+        <Link href="/register">Register</Link>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
