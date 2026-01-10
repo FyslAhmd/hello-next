1c701e02c64edfc0449f3d9276a826dbdae20395
FyslAhmd <info.faysal.32@gmail.com>
2026-01-10 11:52:18 +0600

    add nav

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,16 @@
 import Title from '@/components/Title';
+import Link from 'next/link';
 import React from 'react';
 
 const About = () => {
     return (
         <div>
             <Title>This is about page</Title>
+
+            <nav className='text-xl space-x-5'>
+                <Link href={"/about/contact"}>Contact</Link>
+                <Link href={"/about/teams"}>Teams</Link>
+            </nav>
         </div>
     );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Header = () => {
     return (
-        <div className='px-4 py-3 border-b-2 border-gray-500'>
-            <h2 className='text-xl font-semibold'>Dev Story</h2>
+        <div className='px-4 py-3 border-b-2 border-gray-500 flex items-center justify-between flex-wrap'>
+            <Link href="/" className='text-xl font-semibold'>Dev Story</Link>
+
+            <nav className='text-xl space-x-4'>
+                <Link href="/about">About</Link>
+                <Link href="/stories">Stories</Link>
+                <Link href="/tutorials">Tutorials</Link>
+                <Link href="/login">Login</Link>
+                <Link href="/register">Register</Link>
+            </nav>
         </div>
     );
 };
